3b2d20ad31d25874e9e262bde5fd76adedf3d743
FedericoNicolasMiculan <federiconicolasmiculan>
2023-03-19 20:19:58 -0300

    creo que subi cv

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var imgEmail = document.getElementById("email");
+imgEmail.addEventListener( "click", funcionEmail )
+function funcionEmail(){ alert ("Federicomiculan@gmail.com")
+}
+
+var imgTelefono = document.getElementById("telefono");
+imgTelefono.addEventListener( "click", funcionTelefono)
+function funcionTelefono(){ alert ("0221-555-4890")
+}
+
+var imgNacimiento = document.getElementById("nacimiento");
+imgNacimiento.addEventListener("click", funcionNacimiento)
+function funcionNacimiento(){ alert ("20 de Abril de 1991")
+}
+
+document.getElementById("botonEmail") .click = document.getElementById("email").innerHTML = "federicomiculan@gmail.com"
+    
